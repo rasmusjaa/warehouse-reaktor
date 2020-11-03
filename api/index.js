8f76e2c699b37cf853728dcd62c41c1fcc0ada90
rasmusjaa <rasmus@lense.fi>
2020-11-03 20:59:59 +0200

    port

--- api/index.js
+++ api/index.js
@@ -89,7 +89,7 @@ app.get('/api/accessories', (request, response) => {
 	response.json(accessories)
 })
   
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
